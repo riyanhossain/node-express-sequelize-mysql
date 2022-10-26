4a8cdf9765b40da8adff2c044d1f753287782b47
mehrabriyan <mehraabriyan@gmail.com>
2022-10-26 18:30:25 +0600

    user.model dn

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,14 +1,12 @@
 const db = require("../models/index");
 
-const dbconnection = () => {
-  db.sequelize
-    .sync({ force: false })
-    .then(() => {
-      console.log("conncted to db");
-    })
-    .catch((err) => {
-      console.log("failed to connect");
-    });
+const dbconnection = async () => {
+  try {
+    await db.sequelize.sync({ force: false });
+    console.log("conncted to db");
+  } catch (err) {
+    console.log("connect failed");
+  }
 };
 
 module.exports = {
--- models/users.model.js
+++ models/users.model.js
@@ -2,15 +2,21 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     phone: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
+      unique: true,
     },
-    number: {
-      type: DataTypes.BOOLEAN,
+    role: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: [["admin", "user"]],
+      },
     },
   });
   return User;
